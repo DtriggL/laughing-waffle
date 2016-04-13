46786fe1a3d8846f6f1d61a151a4b24cf5fb3d3a
DtriggL <trigg56@gmail.com>
2016-04-12 21:03:05 -0500

    Begun adding capability to download all crumb information from server.

--- mobile_app/crumble/app/src/main/java/us/trigg/crumble/MainActivity.java
+++ mobile_app/crumble/app/src/main/java/us/trigg/crumble/MainActivity.java
@@ -1,7 +1,9 @@
 package us.trigg.crumble;
 
 import android.app.FragmentManager;
+import android.app.ProgressDialog;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -28,6 +30,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.ClusterManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -56,6 +61,9 @@ public class MainActivity extends AppCompatActivity implements
 
     SupportMapFragment sMapFragment;
 
+    //-----------------------------------------------------------------------------------
+    // Life-cycle Event Handlers
+    //-----------------------------------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,6 +88,62 @@ public class MainActivity extends AppCompatActivity implements
 
     }
 
+
+    //-----------------------------------------------------------------------------------
+    // Map Event Handlers
+    //-----------------------------------------------------------------------------------
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        Log.d(TAG, "on Map Ready, about to set up clusterer");
+        // Add a the markers from the hashmap to the map
+
+        setUpClusterer();
+
+        // Get all of the crumbs from the server and add them to the map
+        new GetAllCrumbs().execute(null, null, null);
+    }
+
+    //-----------------------------------------------------------------------------------
+    // Google API Event Handlers
+    //-----------------------------------------------------------------------------------
+    @Override
+    public void onConnected(Bundle bundle) { }
+
+    @Override
+    public void onConnectionSuspended(int i) { }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) { }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    //-----------------------------------------------------------------------------------
+    // Navigation Drawer Event Handlers
+    //-----------------------------------------------------------------------------------
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -151,45 +215,147 @@ public class MainActivity extends AppCompatActivity implements
         return true;
     }
 
-    /**
-     * Downloads the database of markers from the server and stores them in a database structure.
-     */
-    public void download() {
-        // Attempt to perform the download
-        Log.d(TAG, "download");
-        try {
-            // Open a connection to the PHP server.
-            // Get the response code
-            // Open an input stream as well as a buffered input reader
-            try {
-                // if (responseCode == HttpURLConnection.HTTP_OK) {
-                // Attempt to parse the data from the server
 
-                /* Add the crumb objects, but only add the coordinates, the rating,
-                 * the name, and the number of visits to the local database structure.
-                 */
 
-                // Perhaps add a post method to add the crumbs to the hashmap
-
-                // Post: Notify the UI thread that the markers are ready to be loaded.
-
-            } catch (Exception e) {
-
-            } finally {
-                // Close the input streams
-            }
-        } catch(Exception e) {
-
-        }
-    }
-
+    //-----------------------------------------------------------------------------------
+    // Public Methods
+    //-----------------------------------------------------------------------------------
     public GoogleMap getMap() {
         Log.d(TAG, "map is about to be returned");
         return mMap;
     }
-    //-------------------------------------------------------------------------
+
+
+
+
+    //-----------------------------------------------------------------------------------
+    // Private Methods
+    //-----------------------------------------------------------------------------------
+    private void addCrumbs(JSONObject json) {
+        // 1. Wait for the mMap object to be non-null
+        synchronized (mMap) {
+            try {
+                if (mMap == null) {
+                    Thread.sleep(20);
+                } else {
+                    // 2. Add all of the information from the data JSON object to the map as crumbs
+                    try {
+                        // Check to see if the request was a success
+                        if (json.getString(GetAllCrumbs.TAG_SUCCESS).compareTo("OKAY") == 0) {
+
+                        }
+                    }
+                    catch (JSONException e) {
+
+                    }
+                }
+            } catch (InterruptedException e) {
+
+            }
+        }
+
+
+    }
+
+    private void setUpClusterer() {
+        // Initialize the manager with the context and the map.
+        // (Activity extends context, so we can pass 'this' in the constructor.)
+        mClusterManager = new ClusterManager<CrumbClusterItem>(this, getMap());
+
+        // Point the map's listeners at the listeners implemented by the cluster
+        // manager.
+        getMap().setOnCameraChangeListener(mClusterManager);
+        getMap().setOnMarkerClickListener(mClusterManager);
+        Log.d(TAG, "in clustere, getMap() is run");
+        // Add cluster items (markers) to the cluster manager.
+        addItems();
+    }
+
+    private void addItems() {
+        // Initial test code: add ten objects to the cluster manager
+        for (int i = 0; i < 10; i++) {
+            double offset = i/60d;
+            mClusterManager.addItem(new CrumbClusterItem(new Crumb(new LatLng(5,-5 + offset))));
+        }
+        Log.d(TAG, "All items added to map");
+    }
+
+
+    //-----------------------------------------------------------------------------------
     // Private Classes
-    //-------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
+
+
+    //-----------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
+    private class GetAllCrumbs extends AsyncTask<String, String, String> {
+
+        // Progress Dialog
+        private ProgressDialog pDialog;
+
+        // JSON parser class
+        private JSONParser jsonParser = new JSONParser();
+
+        // JSON Object
+        private JSONObject mJson;
+
+        // Status flag
+        private String status;
+
+        // Status results
+        public static final String TAG_SUCCESS = "status";
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog.setMessage("Retreiving crumbs...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                // Get crumbs by making HTTP request
+                mJson = jsonParser.makeHttpRequest(
+                        WebConstants.URL_ALL_CRUMBS, "GET", null);
+
+                // check your log for json response
+                Log.d("Response", mJson.toString());
+
+                // json success tag
+                status = mJson.getString(TAG_SUCCESS);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            (new Runnable() {
+                public void run() {
+                    /**
+                     * Call function in UI thread to update the  map
+                     * */
+                    addCrumbs(mJson);
+                }
+            }).run();
+        }
+    }
+
+
+
+
+    //-----------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------
     private class MyInfoWindow implements GoogleMap.OnInfoWindowClickListener,
             GoogleMap.InfoWindowAdapter {
         private View myInfoWindowView;
@@ -216,91 +382,6 @@ public class MainActivity extends AppCompatActivity implements
         public View getInfoWindow(Marker marker) {
             return null;
         }
-    }
-
-    //-------------------------------------------------------------------------
-    // Private Methods
-    //-------------------------------------------------------------------------
-    private void downloadCrumbs() {
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
-                download();
-            }
-        };
-        Thread t = new Thread(r);
-        t.start();
-    }
-
-    private void setUpClusterer() {
-        // Initialize the manager with the context and the map.
-        // (Activity extends context, so we can pass 'this' in the constructor.)
-        mClusterManager = new ClusterManager<CrumbClusterItem>(this, getMap());
-
-        // Point the map's listeners at the listeners implemented by the cluster
-        // manager.
-        getMap().setOnCameraChangeListener(mClusterManager);
-        getMap().setOnMarkerClickListener(mClusterManager);
-        Log.d(TAG, "in clustere, getMap() is run");
-        // Add cluster items (markers) to the cluster manager.
-        addItems();
-    }
-
-    private void addItems() {
-        // Initial test code: add ten objects to the cluster manager
-        for (int i = 0; i < 10; i++) {
-            double offset = i/60d;
-            mClusterManager.addItem(new CrumbClusterItem(new Crumb(new LatLng(5,-5 + offset))));
-        }
-        Log.d(TAG, "All items added to map");
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
-        Log.d(TAG, "on Map Ready, about to set up clusterer");
-        // Add a the markers from the hashmap to the map
-
-        setUpClusterer();
-        // Test Code:
-        // Position the map.
-        Log.d(TAG, "set up clusterer");
-        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(5, -5), 10));
-        Log.d(TAG, "getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(5, -5), 10));");
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) { }
-
-    @Override
-    public void onConnectionSuspended(int i) { }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) { }
-
-    @Override
-    public boolean onMarkerClick(Marker marker) {
-        return false;
-    }
-
-    @Override
-    public boolean onMyLocationButtonClick() {
-        return false;
     }
 
 }
---
+++ mobile_app/crumble/app/src/main/java/us/trigg/crumble/WebConstants.java
@@ -0,0 +1,11 @@
+package us.trigg.crumble;
+
+/**
+ * Created by trigglatour on 4/12/16.
+ */
+public class WebConstants {
+    public static final String SERVER_ADDR = "http://132701.ddns.uark.edu/api/";
+
+    public static final String URL_ALL_CRUMBS= SERVER_ADDR + "crumb/all";
+
+}
