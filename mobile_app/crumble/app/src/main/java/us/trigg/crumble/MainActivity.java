aedf63c7fe5dc26407e39e9f26f7f2fad5aeacb2
DtriggL/laughing-waffle <mseera01@gmail.com>
2016-04-12 19:27:10 -0500

    latest manoj fragment stuff

--- mobile_app/crumble/app/src/main/java/us/trigg/crumble/MainActivity.java
+++ mobile_app/crumble/app/src/main/java/us/trigg/crumble/MainActivity.java
@@ -1,57 +1,83 @@
 package us.trigg.crumble;
 
-import android.content.Intent;
+import android.app.FragmentManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.clustering.ClusterManager;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener,
+        OnMapReadyCallback,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMarkerClickListener {
 
     public static final String EXTRA_MESSAGE = "Explore Activity";
     public static final String TAG = "Main Activity";
 
-    ArrayList logEntries;
-    ArrayAdapter logEntriesAdapter;
+    private ClusterManager<CrumbClusterItem> mClusterManager;
+    private HashMap<Marker, Crumb> markers;
+
+    private GoogleMap mMap;
+    protected GoogleApiClient mGoogleApiClient;
+    //Location
+    protected Location mLastLocation;
+    //Addresses
+    protected boolean mAddressRequsted;
+    protected String mAddressOutput;
+
+    SupportMapFragment sMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        logEntries = new ArrayList<String>();
-        logEntriesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
-                logEntries);
-        ListView logBookListView = (ListView) findViewById(R.id.logbookListView);
-        logBookListView.setAdapter(logEntriesAdapter);
+        FragmentManager fm = getFragmentManager();
+        fm.beginTransaction().replace(R.id.frag_manager_frame, new us.trigg.crumble.fragments.LoginFragment()).commit();
 
-        logEntries.add("2/16/16 Discovered: The best fishing spot on Beaver Lake\nuser: tdlatour");
-        logEntries.add("3/02/16 Discovered: hiking must see @ sunset\nuser: jcl006");
-        logEntries.add("3/03/16 Discovered: hiking must see @ sunrise\nuser: jcl006");
-        logEntries.add("3/10/16 Discovered: 11 things you never expected to find in Central Park\nuser: mseeram");
+        sMapFragment = SupportMapFragment.newInstance();
+        sMapFragment.getMapAsync(this);
 
-        logEntriesAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -90,28 +116,191 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
+        FragmentManager fm = getFragmentManager();
+        android.support.v4.app.FragmentManager sFm = getSupportFragmentManager();
+
         int id = item.getItemId();
-        Log.d(TAG, "on navigation item selected");
-        if (id == R.id.explore) {
-            Intent intent = new Intent(this, Explore.class);
-            intent.putExtra(EXTRA_MESSAGE, 1);
-            Log.d(TAG, "explore icon clicked");
-            startActivity(intent);
-        } else if (id == R.id.logbook) {
 
-        } else if (id == R.id.myPins) {
+        //On every menu button click, the map fragment get's hidden
+        if(sMapFragment.isAdded())
+            sFm.beginTransaction().hide(sMapFragment).commit();
 
-        } else if (id == R.id.nearMe) {
+        if (id == R.id.nav_explore) {
 
-        } else if (id == R.id.options) {
-
-        } else if (id == R.id.nav_share) {
+            //The first time, map fragment needs to be added. After that, it just needs to be shown.
+            if (!sMapFragment.isAdded())
+                sFm.beginTransaction().add(R.id.map, sMapFragment).commit();
+            else
+                sFm.beginTransaction().show(sMapFragment).commit();
 
         }
+        else if (id == R.id.nav_logbook) {
+
+            fm.beginTransaction().replace(R.id.frag_manager_frame, new us.trigg.crumble.fragments.LogbookFragment()).commit();
+
+        }
+        else if (id == R.id.nav_myPins) { }
+        else if (id == R.id.nav_nearMe) { }
+        else if (id == R.id.nav_options) { }
+        else if (id == R.id.nav_share) { }
+
         Log.d(TAG, "explore activity finished, or drawer about to close");
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    /**
+     * Downloads the database of markers from the server and stores them in a database structure.
+     */
+    public void download() {
+        // Attempt to perform the download
+        Log.d(TAG, "download");
+        try {
+            // Open a connection to the PHP server.
+            // Get the response code
+            // Open an input stream as well as a buffered input reader
+            try {
+                // if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Attempt to parse the data from the server
+
+                /* Add the crumb objects, but only add the coordinates, the rating,
+                 * the name, and the number of visits to the local database structure.
+                 */
+
+                // Perhaps add a post method to add the crumbs to the hashmap
+
+                // Post: Notify the UI thread that the markers are ready to be loaded.
+
+            } catch (Exception e) {
+
+            } finally {
+                // Close the input streams
+            }
+        } catch(Exception e) {
+
+        }
+    }
+
+    public GoogleMap getMap() {
+        Log.d(TAG, "map is about to be returned");
+        return mMap;
+    }
+    //-------------------------------------------------------------------------
+    // Private Classes
+    //-------------------------------------------------------------------------
+    private class MyInfoWindow implements GoogleMap.OnInfoWindowClickListener,
+            GoogleMap.InfoWindowAdapter {
+        private View myInfoWindowView;
+
+        public MyInfoWindow() {
+            // Add your custom layout here
+            //myInfoWindowView = getLayoutInflater().inflate(R.layout.my_custom_info_layout, null);
+        }
+
+        @Override
+        public void onInfoWindowClick(Marker marker) {
+            Toast.makeText(getApplicationContext(), "Info window clicked",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public View getInfoContents(Marker marker) {
+            // Set the content of the view, then return the view.
+            // Lookup the crumb in the marker hash table.
+            return null;
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            return null;
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    // Private Methods
+    //-------------------------------------------------------------------------
+    private void downloadCrumbs() {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                download();
+            }
+        };
+        Thread t = new Thread(r);
+        t.start();
+    }
+
+    private void setUpClusterer() {
+        // Initialize the manager with the context and the map.
+        // (Activity extends context, so we can pass 'this' in the constructor.)
+        mClusterManager = new ClusterManager<CrumbClusterItem>(this, getMap());
+
+        // Point the map's listeners at the listeners implemented by the cluster
+        // manager.
+        getMap().setOnCameraChangeListener(mClusterManager);
+        getMap().setOnMarkerClickListener(mClusterManager);
+        Log.d(TAG, "in clustere, getMap() is run");
+        // Add cluster items (markers) to the cluster manager.
+        addItems();
+    }
+
+    private void addItems() {
+        // Initial test code: add ten objects to the cluster manager
+        for (int i = 0; i < 10; i++) {
+            double offset = i/60d;
+            mClusterManager.addItem(new CrumbClusterItem(new Crumb(new LatLng(5,-5 + offset))));
+        }
+        Log.d(TAG, "All items added to map");
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        Log.d(TAG, "on Map Ready, about to set up clusterer");
+        // Add a the markers from the hashmap to the map
+
+        setUpClusterer();
+        // Test Code:
+        // Position the map.
+        Log.d(TAG, "set up clusterer");
+        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(5, -5), 10));
+        Log.d(TAG, "getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(5, -5), 10));");
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) { }
+
+    @Override
+    public void onConnectionSuspended(int i) { }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) { }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
 }
---
+++ mobile_app/crumble/app/src/main/java/us/trigg/crumble/fragments/LogbookFragment.java
@@ -0,0 +1,47 @@
+package us.trigg.crumble.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import us.trigg.crumble.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ManojSeeram on 4/11/16.
+ */
+public class LogbookFragment extends Fragment {
+
+    ArrayList logEntries;
+    ArrayAdapter logEntriesAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_logbook, container, false);
+
+        logEntries = new ArrayList<String>();
+        logEntriesAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,
+                logEntries);
+        ListView logBookListView = (ListView) rootView.findViewById(R.id.logbookListView);
+        logBookListView.setAdapter(logEntriesAdapter);
+
+        logEntries.add("2/16/16 Discovered: The best fishing spot on Beaver Lake\nuser: tdlatour");
+        logEntries.add("3/02/16 Discovered: hiking must see @ sunset\nuser: jcl006");
+        logEntries.add("3/03/16 Discovered: hiking must see @ sunrise\nuser: jcl006");
+        logEntries.add("3/10/16 Discovered: 11 things you never expected to find in Central Park\nuser: mseeram");
+
+        logEntriesAdapter.notifyDataSetChanged();
+
+        return rootView;
+
+    }
+
+}
